6acfd08eed67818900166d71f076bd8f950c2e55
mattiaricc05 <m.riccardi@uniandes.edu.co>
2025-05-14 08:54:44 -0500

    avance

--- mriccardi/src/app/pais/pais-detail/pais-detail.component.ts
+++ mriccardi/src/app/pais/pais-detail/pais-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PaisDetail } from '../pais-detail';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../pais.service';
 
 @Component({
   selector: 'app-pais-detail',
@@ -8,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaisDetailComponent implements OnInit {
 
-  constructor() { }
+  paisId!: string;
+  @Input() paisDetail!: PaisDetail;
+
+  constructor(
+    private route: ActivatedRoute,
+    private paisService: PaisService
+  ) { }
+
+  getPais() {
+    this.paisService.getPais(this.paisId).subscribe(apiData => {this.paisDetail = apiData;})
+  }
 
   ngOnInit() {
+    if(this.paisDetail === undefined){
+      this.paisId = this.route.snapshot.paramMap.get('id')!
+      if(this.paisId){
+        this.getPais();
+      }
+    }
   }
 
 }
--- mriccardi/src/app/pais/pais-list/pais-list.component.ts
+++ mriccardi/src/app/pais/pais-list/pais-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisDetail } from '../pais-detail';
+import { PaisService } from '../pais.service';
+import { Pais } from '../pais';
 
 @Component({
   selector: 'app-pais-list',
@@ -8,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaisListComponent implements OnInit {
   paises: Array<PaisDetail> = [];
-  constructor() { }
+  selected: boolean = false;
+  selectedPais!: PaisDetail;
+  searchedPais: any;
+
+  constructor(private paisService: PaisService) { }
+
+  getPaises(): void {
+    this.paisService.getPaises().subscribe({next: apiData => this.paises = apiData });
+  }
+
+  onSelected(pais: PaisDetail): void {
+    this.selected = true;
+    this.selectedPais = pais;
+  }
 
   ngOnInit() {
+    this.getPaises();
   }
 
 }
